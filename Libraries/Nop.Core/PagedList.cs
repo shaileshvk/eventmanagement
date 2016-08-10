16a33c44f2e09d2bf442ee6f837fd3250f565066
shaileshvk <shailesh@insetweb.com>
2016-08-10 15:14:21 +0530

    Add new associate  product changes

---
+++ Libraries/Nop.Core/PagedList.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Nop.Core
+{
+    /// <summary>
+    /// Paged list
+    /// </summary>
+    /// <typeparam name="T">T</typeparam>
+    [Serializable]
+    public class PagedList<T> : List<T>, IPagedList<T> 
+    {
+        /// <summary>
+        /// Ctor (paging in performed inside)
+        /// </summary>
+        /// <param name="source">source</param>
+        /// <param name="pageIndex">Page index</param>
+        /// <param name="pageSize">Page size</param>
+        public PagedList(IEnumerable<T> source, int pageIndex, int pageSize)
+        {
+            Init(source, pageIndex, pageSize);
+        }
+
+        /// <summary>
+        /// Ctor (already paged soure is passed)
+        /// </summary>
+        /// <param name="source">source</param>
+        /// <param name="pageIndex">Page index</param>
+        /// <param name="pageSize">Page size</param>
+        /// <param name="totalCount">Total count</param>
+        public PagedList(IEnumerable<T> source, int pageIndex, int pageSize, int totalCount)
+        {
+            Init(source, pageIndex, pageSize, totalCount);
+        }
+
+        /// <summary>
+        /// Initialize
+        /// </summary>
+        /// <param name="source">source</param>
+        /// <param name="pageIndex">Page index</param>
+        /// <param name="pageSize">Page size</param>
+        /// <param name="totalCount">Total count</param>
+        private void Init(IEnumerable<T> source, int pageIndex, int pageSize, int? totalCount = null)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+            if (pageSize <= 0)
+                throw new ArgumentException("pageSize must be greater than zero");
+
+            TotalCount = totalCount ?? source.Count();
+            TotalPages = TotalCount / pageSize;
+
+            if (TotalCount % pageSize > 0)
+                TotalPages++;
+
+            PageSize = pageSize;
+            PageIndex = pageIndex;
+            source = totalCount == null ? source.Skip(pageIndex * pageSize).Take(pageSize) : source;
+            AddRange(source);
+        }
+
+        public int PageIndex { get; private set; }
+        public int PageSize { get; private set; }
+        public int TotalCount { get; private set; }
+        public int TotalPages { get; private set; }
+
+        public bool HasPreviousPage
+        {
+            get { return (PageIndex > 0); }
+        }
+        public bool HasNextPage
+        {
+            get { return (PageIndex + 1 < TotalPages); }
+        }
+    }
+}
